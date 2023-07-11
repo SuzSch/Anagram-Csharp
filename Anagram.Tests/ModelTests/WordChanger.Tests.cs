d434404d728eb68dcdb9f65f02d7ae5460debc47
Jason <elijahchurch@gmail.com>
2023-07-11 15:06:28 -0700

    finished WordChange class. Complete UI logic Co-authored by: Suzanne Schuber suzanneschuber@gmail.com Co-authored by: Hernan Verar hernanverar@gmail.com

--- Anagram.Tests/ModelTests/WordChanger.Tests.cs
+++ Anagram.Tests/ModelTests/WordChanger.Tests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
 using System.Collections.Generic;
-using System;
+
 
 namespace Anagram.Tests
 {
@@ -15,5 +15,28 @@ namespace Anagram.Tests
             char[] Result = WordChanger.ChangeWordToArray(userInput);
             Assert.AreEqual(typeof(char[]), Result.GetType());
         }
+
+        [TestMethod]
+        public void ChangeWordToArray_SortArray_Array()
+        {
+            string userInput = "gig";
+            char[] Result = WordChanger.ChangeWordToArray(userInput);
+            char[] expectedResult = { 'g', 'g', 'i' };
+            CollectionAssert.AreEqual(expectedResult, Result);
+        }
+
+        [TestMethod]
+        public void StoreMainWord_StoreWord_Void()
+        {
+            string userInput = "cat";
+            char[] catSorted = { 'a', 'c', 't' };
+            WordChanger.StoreMainWord(userInput);
+            Dictionary<string, char[]> Result = WordChanger.mainWordDictionary;
+            Dictionary<string, char[]> expectedResult = new Dictionary<string, char[]>() { { "cat", catSorted }, };
+            CollectionAssert.AreEqual(expectedResult["cat"], Result[userInput]);
+            // CollectionAssert.AreEqual(expectedResult.Values, Result.Values);
+        }
+
+
     }
 }
--- Anagram/Models/WordChanger.cs
+++ Anagram/Models/WordChanger.cs
@@ -1,15 +1,32 @@
 using System.Collections.Generic;
 using System;
 
+
 namespace Anagram.Models
 {
     public class WordChanger
     {
+        public static Dictionary<string, char[]> mainWordDictionary = new Dictionary<string, char[]>() { };
+        public static Dictionary<string, char[]> listDictionary = new Dictionary<string, char[]>() { };
         public static char[] ChangeWordToArray(string userInput)
         {
             string newWord = userInput.ToLower();
             char[] wordArray = newWord.ToCharArray();
+            Array.Sort(wordArray);
             return wordArray;
         }
+
+        public static void StoreMainWord(string mainword)
+        {
+            char[] wordArray = WordChanger.ChangeWordToArray(mainword);
+            mainWordDictionary.Add(mainword, wordArray);
+        }
+
+        public static void StoreListWords(string listword)
+        {
+            char[] wordArray = WordChanger.ChangeWordToArray(listword);
+            listDictionary.Add(listword, wordArray);
+        }
+
     }
 }
