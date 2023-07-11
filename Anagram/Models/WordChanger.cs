bb21ce590efbad4ef2ce31a5b5631384121ccf4b
Jason <elijahchurch@gmail.com>
2023-07-11 09:19:41 -0700

    rename files and folders Co-authored by: Suzanne Schuber suzanneschuber@gmail.com Co-authored by: Hernan Verar hernanverar@gmail.com

---
+++ Anagram/Models/WordChanger.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Anagram.Models
+{
+    public class WordChanger
+    {
+        // Business Logic goes here.
+    }
+}
---
+++ Anagram/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Anagram.Models;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main()
+        {
+            // UI code goes here.
+        }
+    }
+}
